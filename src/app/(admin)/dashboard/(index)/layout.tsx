bfdb05f4fcbfde6cb266d4758413e68445d63faf
pasipasipada <irfanoputra27@gmail.com>
2025-02-22 22:36:12 +0700

    feat: add slicing dashboard page

--- src/app/(admin)/dashboard/(index)/layout.tsx
+++ src/app/(admin)/dashboard/(index)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../../../globals.css";
+import Header from "./components/header";
+import Sidebar from "./components/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +26,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <div className="flex min-h-screen w-full flex-col bg-muted/40">
+          <Sidebar />
+          <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+            <Header />
+            <main className="py-4 px-6">
+              {children}
+            </main>
+          </div>
+        </div>
       </body>
     </html>
   );
