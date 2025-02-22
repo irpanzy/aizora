eeb158a0a96c63a7c851be366c59b239dc9eca56
pasipasipada <irfanoputra27@gmail.com>
2025-02-22 23:21:46 +0700

    feat: add function logout

---
+++ src/app/(admin)/dashboard/(index)/lib/action.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { getUser, lucia } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { ActionResult } from "@/types";
+import { cookies } from "next/headers";
+
+export async function Logout(
+  _: unknown,
+  formData: FormData
+): Promise<ActionResult> {
+  const { session } = await getUser();
+
+  if (!session) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  await lucia.invalidateSession(session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+
+  (await cookies()).set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+
+  return redirect("/dashboard/sign-in");
+}
