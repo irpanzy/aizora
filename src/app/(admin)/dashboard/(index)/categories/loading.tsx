648fbd04685be7d3a2eb9a5c42ca3226b32d2272
pasipasipada <irfanoputra27@gmail.com>
2025-02-26 00:21:01 +0700

    fix: loading skeleton

--- src/app/(admin)/dashboard/(index)/categories/loading.tsx
+++ src/app/(admin)/dashboard/(index)/categories/loading.tsx
@@ -8,51 +8,51 @@ export default function Loading() {
                 <Skeleton className="h-8 w-32" />
             </div>
             <div className="space-x-4 inline-flex">
-                <div className="flex flex-col space-y-3 p-6">
+                <div className="flex flex-col space-y-3 p-4">
                     <Skeleton className="h-8 w-[125px]" />
                     <Skeleton className="h-4 w-[350px]" />
                     <div className="flex flex-col space-y-3 p-4">
                         <Skeleton className="h-10 w-[125px]" />
                         <div className="flex space-x-4">
-                            <Skeleton className="h-12 w-[530px]" />
+                            <Skeleton className="h-14 w-[530px]" />
                             <div className="flex space-x-4">
-                                <Skeleton className="h-12 w-[50px]" />
-                                <Skeleton className="h-12 w-[100px]" />
+                                <Skeleton className="h-14 w-[50px]" />
+                                <Skeleton className="h-14 w-[100px]" />
                             </div>
                         </div>
                         <div className="flex space-x-4">
-                            <Skeleton className="h-12 w-[530px]" />
+                            <Skeleton className="h-14 w-[530px]" />
                             <div className="flex space-x-4">
-                                <Skeleton className="h-12 w-[50px]" />
-                                <Skeleton className="h-12 w-[100px]" />
+                                <Skeleton className="h-14 w-[50px]" />
+                                <Skeleton className="h-14 w-[100px]" />
                             </div>
                         </div>
                         <div className="flex space-x-4">
-                            <Skeleton className="h-12 w-[530px]" />
+                            <Skeleton className="h-14 w-[530px]" />
                             <div className="flex space-x-4">
-                                <Skeleton className="h-12 w-[50px]" />
-                                <Skeleton className="h-12 w-[100px]" />
+                                <Skeleton className="h-14 w-[50px]" />
+                                <Skeleton className="h-14 w-[100px]" />
                             </div>
                         </div>
                         <div className="flex space-x-4">
-                            <Skeleton className="h-12 w-[530px]" />
+                            <Skeleton className="h-14 w-[530px]" />
                             <div className="flex space-x-4">
-                                <Skeleton className="h-12 w-[50px]" />
-                                <Skeleton className="h-12 w-[100px]" />
+                                <Skeleton className="h-14 w-[50px]" />
+                                <Skeleton className="h-14 w-[100px]" />
                             </div>
                         </div>
                         <div className="flex space-x-4">
-                            <Skeleton className="h-12 w-[530px]" />
+                            <Skeleton className="h-14 w-[530px]" />
                             <div className="flex space-x-4">
-                                <Skeleton className="h-12 w-[50px]" />
-                                <Skeleton className="h-12 w-[100px]" />
+                                <Skeleton className="h-14 w-[50px]" />
+                                <Skeleton className="h-14 w-[100px]" />
                             </div>
                         </div>
                         <div className="flex space-x-4">
-                            <Skeleton className="h-12 w-[530px]" />
+                            <Skeleton className="h-14 w-[530px]" />
                             <div className="flex space-x-4">
-                                <Skeleton className="h-12 w-[50px]" />
-                                <Skeleton className="h-12 w-[100px]" />
+                                <Skeleton className="h-14 w-[50px]" />
+                                <Skeleton className="h-14 w-[100px]" />
                             </div>
                         </div>
                     </div>
