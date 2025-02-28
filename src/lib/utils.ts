d1a5316673918d8e2c6684d4f09454f3f6817f7e
pasipasipada <irfanoputra27@gmail.com>
2025-02-28 15:13:30 +0700

    feat: add utility functions for currency and date formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import dayjs from "dayjs";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function rupiahFormat(value: number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(value);
+}
+
+export function dateFormat(date: Date | null, format = "DD-MM-YYYY") {
+  if (!date) {
+    return dayjs().format(format);
+  }
+
+  return dayjs(date).format(format);
 }
