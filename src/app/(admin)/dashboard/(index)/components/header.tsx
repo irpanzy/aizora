96a72c30937feb8713690cc1217141f9d90ed340
pasipasipada <irfanoputra27@gmail.com>
2025-03-02 21:45:46 +0700

    refactor: simplify header by removing breadcrumb and search

--- src/app/(admin)/dashboard/(index)/components/header.tsx
+++ src/app/(admin)/dashboard/(index)/components/header.tsx
@@ -1,44 +1,11 @@
 import React from "react";
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Input } from "@/components/ui/input";
-import Link from "next/link";
-import { Search } from "lucide-react";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export default function Header() {
     return (
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-            <Breadcrumb className="hidden md:flex">
-                <BreadcrumbList>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink asChild>
-                            <Link href="/dashboard">Dashboard</Link>
-                        </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbItem>
-                        <BreadcrumbPage>Home</BreadcrumbPage>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
             <div className="relative ml-auto flex items-center">
                 <ModeToggle />
-
-                <div className="relative ml-3">
-                    <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                    <Input
-                        type="search"
-                        placeholder="Search..."
-                        className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-                    />
-                </div>
             </div>
         </header>
     );
