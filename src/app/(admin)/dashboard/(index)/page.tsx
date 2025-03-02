29866a3fceed02762387bd9612eb6c6364b7e0d8
pasipasipada <irfanoputra27@gmail.com>
2025-03-02 21:42:26 +0700

    feat: add breadcrumb navigation to dashboard

--- src/app/(admin)/dashboard/(index)/page.tsx
+++ src/app/(admin)/dashboard/(index)/page.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage } from '@/components/ui/breadcrumb';
 
 export default function DashboardPage() {
   return (
-    <div>
-      Dashboard Page
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <Breadcrumb className="flex">
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbPage>Dashboard</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
     </div>
   )
 }
