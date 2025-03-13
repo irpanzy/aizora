49391bc26cd44da685f73cd02478c33863291f9a
pasipasipada <irfanoputra27@gmail.com>
2025-03-14 00:08:58 +0700

    feat: add font and metadata

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,36 @@
 import React from "react";
 import styles from "./not-found.module.scss";
 import Image from "next/image";
+import { Montserrat } from "next/font/google";
+import { Metadata } from "next";
 
-export default function NotFoundPage() {
+const montserrat = Montserrat({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin-ext']
+})
+
+export const metadata: Metadata = {
+  title: "Page Not Found",
+  icons: {
+    icon: "/assets/logos/404.ico",
+  },
+};
+
+export default function NotFoundPage({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <div className={styles.error}>
-      <Image
-        className={styles.error__image}
-        src="/assets/icons/404.svg"
-        alt="404"
-        width={400}
-        height={400}
-      />
-      <div className={styles.error__title}>Oops! Page Not Found</div>
-    </div>
+    <html lang="en">
+      <body className={montserrat.className}>{children}
+        <div className={styles.error}>
+          <Image
+            className={styles.error__image}
+            src="/assets/icons/404.svg"
+            alt="404"
+            width={400}
+            height={400}
+          />
+          <div className={styles.error__title}>Oops! Page Not Found</div>
+        </div>
+      </body>
+    </html>
   );
 }
