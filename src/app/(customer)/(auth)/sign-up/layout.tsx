ade009ecc712a6c5679c510b656f07c467a78125
pasipasipada <irfanoputra27@gmail.com>
2025-03-19 23:13:43 +0700

    feat: add layout & metadata

---
+++ src/app/(customer)/(auth)/sign-up/layout.tsx
@@ -0,0 +1,24 @@
+import { Montserrat } from "next/font/google";
+
+import '../../../index.css'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: "Sign Up | Aizora",
+    icons: {
+        icon: "/assets/logos/favicon.ico",
+    },
+};
+
+const montserrat = Montserrat({
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    subsets: ['latin-ext']
+})
+
+export default function SignUpLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
+    return (
+        <html lang="en">
+            <body className={montserrat.className}>{children}</body>
+        </html>
+    )
+}
