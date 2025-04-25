5035523215a6ecacf2c20f3914b5fedb32e95f36
pasipasipada <irfanoputra27@gmail.com>
2025-04-26 02:56:31 +0700

    fix: form sign in

--- src/app/(customer)/(auth)/sign-in/components/form-sign-in.tsx
+++ src/app/(customer)/(auth)/sign-in/components/form-sign-in.tsx
@@ -55,47 +55,6 @@ export default function FormSignIn() {
     return (
         <div id="signin" className="bg-[#AC1754] min-h-screen py-5 px-5 flex flex-col">
             <ToastContainer position="top-right" autoClose={2000} />
-            {/* <nav className="container max-w-[1130px] mx-auto flex items-center justify-between bg-[#AC1754] p-5 rounded-3xl">
-                <div className="flex shrink-0">
-                    <Image
-                        alt="Logo"
-                        src="/assets/logos/logo.svg"
-                        width={100}
-                        height={100}
-                    />
-                </div>
-                <ul className="flex items-center gap-[30px]">
-                    <li className="hover:font-bold hover:text-[#F7A8C4] transition-all duration-300 text-white">
-                        <a href="index.html">Shop</a>
-                    </li>
-                    <li className="hover:font-bold hover:text-[#F7A8C4] transition-all duration-300 text-white">
-                        <a href="">Categories</a>
-                    </li>
-                    <li className="hover:font-bold hover:text-[#F7A8C4] transition-all duration-300 text-white">
-                        <a href="">Testimonials</a>
-                    </li>
-                    <li className="hover:font-bold hover:text-[#F7A8C4] transition-all duration-300 text-white">
-                        <a href="">Rewards</a>
-                    </li>
-                </ul>
-                <div className="flex items-center gap-3">
-                    <a href="cart.html">
-                        <div className="w-12 h-12 flex shrink-0">
-                            <BaggageClaim
-                                className="w-15 h-15 rounded-full bg-white p-3"
-                                width={50}
-                                height={50}
-                            />
-                        </div>
-                    </a>
-                    <a href="signin.html" className="p-[12px_20px] bg-white rounded-full font-semibold">
-                        Sign In
-                    </a>
-                    <a href="signup.html" className="p-[12px_20px] bg-white rounded-full font-semibold">
-                        Sign Up
-                    </a>
-                </div>
-            </nav> */}
             <motion.div
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
